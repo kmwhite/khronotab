c9e9939626f23df758e6d366779477197955feb0
Kristofer M White <me@kmwhite.net>
2011-08-27 14:47:10 -0500

    Cleanup main test

--- spec/khronotab_spec.rb
+++ spec/khronotab_spec.rb
@@ -1,7 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Khronotab" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Khronotab do
+  before do
+  end
+  it 'should load files' do
+
   end
 end
